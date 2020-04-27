9cd195cc1dad270af8345f3adda19a6a4b5716d7
Reine0017 <m.fortitudo.fr@gmail.com>
2020-04-27 21:22:23 +0800

    clean up

--- callScriptTest.py
+++
@@ -1,8 +0,0 @@
-import neuralStyleProcess
-
-def testingFunction(directoryName, filename):
-	neuralStyleProcess.neuralStyleTransfer(directoryName,filename)
-
-
-if __name__=='__main__':
-	testingFunction('/Users/fangran/Documents/pyFlaskCV/images/', 'bbq.jpg')
